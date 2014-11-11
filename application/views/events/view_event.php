888709b74556ebb7d2adf00bc3929b119feefc39
osmiumwo1f <osmiumwo1f@outlook.com>
2014-11-11 14:07:44 +0700

    Rewrite .htaccess and continue restoring

--- application/views/events/view_event.php
+++ application/views/events/view_event.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="<?php echo $burl;?>assets/upload.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/upload.js"></script>
 <div id="shade"></div>
 <div id="modal">
     <?php echo form_open_multipart('events/upload_file');?>
@@ -18,7 +18,6 @@
     <button class="close">ปิด</button>
 </div>
     <?php
-<<<<<<< HEAD
 echo '<h2>'.$e_item[1]['name'].'</h2><br>';
 echo '1: '.$e_item[1]['descriptions'].'<br>';
 echo '2: '.$e_item[1]['requirements'].'<br>';
@@ -40,26 +39,3 @@ echo '7: '.$e_item[1]['note'].'<br>';
 </table>
 <button id="file" class="add">เพิ่มไฟล์</button>
 <a href="<?php echo $e_item[1]['id'];?>/satis/create/">สร้างแบบประเมินความพึงพอใจ</a>
-=======
-    echo '<h2>'.$e_item[1]['name'].'</h2><br>';
-    echo '1: '.$e_item[1]['descriptions'].'<br>';
-    echo '2: '.$e_item[1]['requirements'].'<br>';
-    echo '3: '.$e_item[1]['fee'].'<br>';
-    echo '4: '.$e_item[1]['date'].'<br>';
-    echo '5: '.$e_item[1]['place'].'<br>';
-    echo '6: '.$e_item[1]['amount'].'<br>';
-    echo '7: '.$e_item[1]['note'].'<br>';
-    ?>
-    <table class="event">
-        <th>ชื่อไฟล์</th>
-        <th>รายละเอียด</th>
-        <?php foreach ($e_item as $f):?>
-            <tr>
-                <td><?php echo $f['fn']; ?></td>
-                <td><?php echo $f['fd']; ?></td>
-            </tr>
-        <?php endforeach ?>
-    </table>
-    <a href="satis/create/">สร้างแบบประเมินความพึงพอใจ</a>
-</div>
->>>>>>> parent of 4a9c1cb... Glue all functions together
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,15 +5,15 @@
 <html>
 <head>
 	<title><?php echo $title ?></title>
-    <script src="<?php echo $burl;?>assets/jquery-2.1.1.js"></script>
+    <script src="<?php echo base_url();?>assets/jquery-2.1.1.js"></script>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="<?php echo $burl;?>assets/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/style.css">
 </head>
 <body>
 	<h1 align="center">ITS</h1>
 	<div id="menubar">
-		<div><a href="<?php echo $burl;?>"></a></div>
-		<div><a href="<?php echo $burl;?>"></a></div>
-		<div><a href="<?php echo $burl;?>"></a></div>
+		<div><a href="<?php echo base_url();?>"></a></div>
+		<div><a href="<?php echo base_url();?>"></a></div>
+		<div><a href="<?php echo base_url();?>"></a></div>
 	</div>
     <div class="content">
